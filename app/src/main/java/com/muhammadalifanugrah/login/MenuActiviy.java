8e6c6378d26cbe925b6d5805d701e9a42006b293
sekcess <itsalifanugrah@gmail.com>
2023-07-06 11:03:35 +0700

    Login Part A

---
+++ app/src/main/java/com/muhammadalifanugrah/login/MenuActiviy.java
@@ -0,0 +1,14 @@
+package com.muhammadalifanugrah.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActiviy extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_menu);
+    }
+}
