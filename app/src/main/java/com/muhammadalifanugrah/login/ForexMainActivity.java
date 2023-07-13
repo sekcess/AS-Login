d1db5776c3036977941fbd19718b7fa85c6af4d2
sekcess <itsalifanugrah@gmail.com>
2023-07-13 10:43:18 +0700

    Forex Part C

---
+++ app/src/main/java/com/muhammadalifanugrah/login/ForexMainActivity.java
@@ -0,0 +1,14 @@
+package com.muhammadalifanugrah.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForexMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forex_main);
+    }
+}
